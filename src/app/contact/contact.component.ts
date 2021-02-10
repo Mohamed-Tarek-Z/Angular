1ca2ffa242a346bd9fe534bb7e3418464db096fc
Mohamed Tarek <MohamedTarek153@gmail.com>
2021-02-10 18:30:53 +0200

    reactive forms with valid p3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,6 +8,28 @@ import { ContactType, Feedback } from '../shared/feedback';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  validationMessages = {
+    firstname: {
+      required: 'First Name is required.',
+      minlength: 'First Name must be at least 2 characters long.',
+      maxlength: 'FirstName cannot be more than 25 characters long.'
+    },
+    lastname: {
+      required: 'Last Name is required.',
+      minlength: 'Last Name must be at least 2 characters long.',
+      maxlength: 'Last Name cannot be more than 25 characters long.'
+    },
+    telnum: {
+      required: 'Tel. number is required.',
+      pattern: 'Tel. number must contain only numbers.'
+    },
+    email: {
+      required: 'Email is required.',
+      email: 'Email not in valid format.'
+    },
+  };
+
   feedbackForm!: FormGroup;
 
   feedback!: Feedback;
@@ -24,15 +46,16 @@ export class ContactComponent implements OnInit {
 
   createForm(): void {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern('[0-9]*')]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
   }
+
   onSubmit(): void {
     this.feedback = this.feedbackForm?.value;
     console.log(this.feedback);
@@ -47,4 +70,5 @@ export class ContactComponent implements OnInit {
     });
     this.feedbackFormDirective.resetForm();
   }
+
 }
